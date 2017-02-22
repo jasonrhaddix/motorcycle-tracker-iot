c9e73db5ca12de7f2bf95dedb07aced20e264131
Jason Haddix <jasonrhaddix@gmail.com>
2017-02-22 14:09:36 -0800

    Added libs and TIME definition

--- app_particle/src/KTM_Tracker.ino
+++ app_particle/src/KTM_Tracker.ino
@@ -1,3 +1,25 @@
+/*****************************************************************/
+/*****************************************************************/
+/*
+	Motocycle / Car Tracker firmware for Particle Electron
+	(c) Copyright 2017, Jason R. Haddix
+
+*/
+/*****************************************************************/
+/*****************************************************************/
+
+#include "_libs/AssetTracker/AssetTracker.h"
+#include "math.h"
+
+
+
+
+// DEFINITIONS
+#define TIME millis()                                // Global system run time - milliseconds
+
+
+
+
 
 void setup()
 {
